8b4d4e6327446cdb9516e09228d44fc9aca1ba57
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-04-03 19:51:46 -0400

    Capture captions prelude as well

--- src/recap/caption.clj
+++ src/recap/caption.clj
@@ -10,12 +10,14 @@
 
 
 
+(s/def ::prelude (s/coll-of string?))
 (s/def ::duration #(re-find #"\d\d:\d\d:\d\d[,.]\d+" %))
 (s/def ::start ::duration)
 (s/def ::end ::duration)
 (s/def ::lines (s/coll-of string?))
 (s/def ::cue (s/keys :req-un [::start ::end ::lines]))
-(s/def ::caption (s/coll-of ::cue))
+(s/def ::cues (s/coll-of ::cue))
+(s/def ::caption (s/keys :req-un [::prelude ::cues]))
 
 (s/fdef parse
         :args (s/cat :input string?)
@@ -40,30 +42,51 @@
 
     :else
     (loop [line-idx 0
+           captured-prelude? false
+           prelude []
            scanning-for :time-range
            cues []]
       (if (>= line-idx num-lines)
-        cues
+        ;; Ignore prelude if empty or is not separated by an empty line from
+        ;; the cues
+        (let [prelude? (and (not (empty? prelude))
+                            (-> prelude last empty?))]
+          {:prelude (if prelude? prelude [])
+           :cues cues})
         (let [line (nth lines line-idx)]
           (if (= scanning-for :time-range)
             ;; Scan for line specifying time range:
             (let [time-range (parse-time-range line)]
               (if (empty? time-range)
+                (if captured-prelude?
+                  (recur (inc line-idx)
+                         true
+                         prelude
+                         :time-range
+                         cues)
+                  (recur (inc line-idx)
+                         false
+                         (conj prelude line)
+                         :time-range
+                         cues))
                 (recur (inc line-idx)
-                       :time-range
-                       cues)
-                (recur (inc line-idx)
+                       true
+                       prelude
                        :content
                        (conj cues time-range))))
             ;; Otherwise we scan for content/cues:
             (let [content line]
               (if (str/blank? content)
                 (recur (inc line-idx)
+                       true
+                       prelude
                        :time-range
                        cues)
                 (let [total-cues (count cues)
                       cue-to-update (last cues)]
                   (recur (inc line-idx)
+                         true
+                         prelude
                          :content
                          (assoc cues
                                 (dec total-cues)
