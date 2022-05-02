1b2dd0e87cd76fe8cde6f13f1967635498667c27
Thiru Thirunavukarasu <thiru0130@gmail.com>
2022-05-01 22:09:06 -0400

    Speaker tag regex more liberal

--- src/recap/caption/utils.clj
+++ src/recap/caption/utils.clj
@@ -10,16 +10,24 @@
 (defn get-speaker-tag
   "Find speaker tag in the given text.
 
-  A speaker tag is taken to be the text that indentifies a speaker. It should
-  be some capitalised text followed by a colon, at the start of a cue.
+  A speaker tag is taken to be the text that indentifies a speaker.
+
+  It should be some capitalised text followed by a colon, at the beginning of a
+  line with at most one space.
 
   Returns the speaker tag (including the colon) if found, otherwise `nil`."
   [text]
   (some->> text
-           (re-find #"^([A-Z]\w*:)")
-           second))
+           (re-find #"^([A-Z][\w-]*)(\s+[A-Z][\w-]*)?(:)")
+           first))
 
 
 
 (comment
-  (get-speaker-tag "Q1:"))
+  (get-speaker-tag "Q1: hello")
+  (get-speaker-tag "Bob: hello")
+  (get-speaker-tag "Bob-Tim: hello")
+  (get-speaker-tag "Bob Tim: hello")
+  (get-speaker-tag "Bob Tim Li: hello")
+  (get-speaker-tag "Hi, Bob: hello"))
+
