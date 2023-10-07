28b1de0d01a16997ee558c85b360dcea9bf8c54a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2023-10-07 14:34:09 -0400

    Fix missing namespace

--- dev/utils/printing.clj
+++ dev/utils/printing.clj
@@ -3,8 +3,9 @@
   "Various print debugging utilities."
 
   (:require
-            [clojure.spec.alpha :as s]
-            [expound.alpha :as expound]))
+    [clojure.spec.alpha :as s]
+    [expound.alpha :as expound]
+    [puget.printer :as puget]))
 
 (defmacro PP
   "Convenience macro to pretty-print last evaluated result at the REPL."
